8f74fb206cffe725517e581c7082a8a4e060eadd
hz <dxp@ngs.ru>
2016-05-17 22:09:08 +0600

    redesign library merge script

--- merge-lib.sh
+++ merge-lib.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-cat lib-head *.cmp lib-tail > lib.lib
-
-if [ $1 ]; then
-    mv lib.lib $1
+if [ -z $1 ]; then
+    echo 'No library name specified'
+    return
 fi
+
+echo 'EESchema-LIBRARY Version 2.3' > $1.lib
+echo '#encoding utf-8' >> $1.lib
+cat *.cmp >> $1.lib
+echo '#' >> $1.lib
+echo '#End Library' >> $1.lib
+
+echo 'EESchema-DOCLIB  Version 2.0' > $1.dcm
+cat *.dcmp >> $1.dcm
+echo '#' >> $1.dcm
+echo '#End Doc Library' >> $1.dcm
+
+echo "Create $1.lib"
+  
