e17411029df1b214a2cbeb7810d53e36621ec464
hz <dxp@ngs.ru>
2016-06-06 12:01:43 +0600

    add CmpTab group box, rearrage layout

--- kge.py
+++ kge.py
@@ -4,8 +4,9 @@
 
 import re
 import sys
-from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit,
-    QTextEdit, QGridLayout, QTableWidget, QTableWidgetItem, QHeaderView, QApplication)
+from PyQt5.Qt import Qt
+from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit, QPushButton, QGroupBox,
+    QTextEdit, QVBoxLayout, QGridLayout, QTableWidget, QTableWidgetItem, QAbstractItemView, QHeaderView, QApplication)
 
 #-------------------------------------------------------------------------------
 class MainForm(QWidget):
@@ -18,30 +19,61 @@ class MainForm(QWidget):
         
     def initUI(self):
         
-        self.Layout = QGridLayout(self)
+        #----------------------------------------------------
+        #
+        #    Layouts
+        #
+        Layout = QGridLayout(self)
+        self.setLayout(Layout)
+        self.CmpTabBox    = QGroupBox('Components', self)
+        self.CmpTabLayout = QVBoxLayout(self)
+        
+        #----------------------------------------------------
+        #
+        #    Components table
+        #
         self.CmpTable = QTableWidget(0, 2, self)
+        
+        self.CmpTable.cellActivated.connect(self.cellActivated)
+        
+        self.CmpTable.setSelectionBehavior(QAbstractItemView.SelectRows)  # select whole row
+        self.CmpTable.setEditTriggers(QAbstractItemView.NoEditTriggers)   # disable edit cells
         
         self.CmpTable.horizontalHeader().setSectionResizeMode(0, QHeaderView.Interactive)
         self.CmpTable.horizontalHeader().setSectionResizeMode(1, QHeaderView.Interactive)
         self.CmpTable.verticalHeader().setDefaultSectionSize(20)
+        self.CmpTable.verticalHeader().setSectionResizeMode(QHeaderView.Fixed)
         self.CmpTable.setHorizontalHeaderLabels( ('Ref', 'Name') )
-        
-        self.Layout.addWidget(self.CmpTable, 0, 0)
         
         b   = read_file('det-1/det-1.sch')
         rcl = raw_cmp_list(b)
         self.CmpDict = cmp_dict(rcl)
         self.update_cmp_list(self.CmpDict)
         self.CmpTable.show()
+        #----------------------------------------------------
         
+        self.Button = QPushButton('Apply', self)
         
+        self.CmpTabLayout.addWidget(self.CmpTable)
+        self.CmpTabLayout.addWidget(self.Button)
+        self.CmpTabBox.setLayout(self.CmpTabLayout)
+        self.layout().addWidget(self.CmpTabBox, 0,0)
+        
+                
+        #----------------------------------------------------
+        #
+        #    Window
+        #
         self.setWindowTitle('KiCad Schematic Component Manager')
         self.setGeometry(100, 100, 1024, 768)
         self.show()
         
-        #self.CmpTable.
         
-        
+    def cellActivated(self, row, col):
+        items = self.CmpTable.selectedItems()
+        for i in items:
+            if i.column() == 0:
+                print( i.data(Qt.DisplayRole) )
     
     def update_cmp_list(self, cd):
         
