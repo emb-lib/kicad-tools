6c2e6542289057e583d32e1ade276014a3cfdbb3
hz <dxp@ngs.ru>
2016-11-21 15:23:23 +0700

    add titles

--- scmgr.py
+++ scmgr.py
@@ -1,6 +1,36 @@
 #!/usr/bin/python3
 # coding: utf-8
 
+#-------------------------------------------------------------------------------
+#
+#    Project: KiCad Tools
+#    
+#    Name:    KiCad Schematic Component Manager
+#   
+#    Purpose: Main program file      
+#
+#    Copyright (c) 2016, emb-lib Project Team
+#
+#    Permission is hereby granted, free of charge, to any person
+#    obtaining  a copy of this software and associated documentation
+#    files (the "Software"), to deal in the Software without restriction,
+#    including without limitation the rights to use, copy, modify, merge,
+#    publish, distribute, sublicense, and/or sell copies of the Software,
+#    and to permit persons to whom the Software is furnished to do so,
+#    subject to the following conditions:
+#
+#    The above copyright notice and this permission notice shall be included
+#    in all copies or substantial portions of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#    EXPRESS  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+#    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+#    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+#    THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+#-------------------------------------------------------------------------------
 
 import sys
 import os
@@ -612,9 +642,7 @@ class MainWindow(QMainWindow):
         self.Selector.select_comps_signal.connect(self.CmpTable.select_comps_slot)
 
         self.Inspector.load_field.connect(self.FieldInspector.load_field_slot)
-        #self.Inspector.data_changed.connect(self.data_changed_slot)
         self.Inspector.update_comps.connect(self.data_changed_slot)
-        #self.Inspector.data_changed.connect(self.CmpTable.update_cmp_list_slot)
         self.Inspector.update_comps.connect(self.CmpTable.update_cmp_list_slot)
         self.FieldInspector.data_changed.connect(self.data_changed_slot)
         CmpMgr.file_saved.connect(self.file_saved_slot)
